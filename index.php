467bf2e019c24d6bd7819c90008c0cdf70fa0c5f
emryslou <1065873330@qq.com>
2014-03-23 15:01:17 +0800

    添加getPowerlist函数，同时删除了测试代码

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-<?php phpinfo(); ?>
+<?php
+
+function getPowerlist($a,$level = true){
+	$power_name = array(
+			'1' => 'select',
+			'2' => 'insert',
+			'4' => 'update',
+			'8' => 'delete',
+		);
+	$powers = array();
+	$sum = 0;
+	//==================================================
+	$power_num = 4;
+	for($i = $power_num -1;$i >= 0;$i--) {
+		$twopow = pow(2,$i);
+		
+		if($sum + $twopow <= $a){
+			$powers[] = $power_name[$twopow];
+			$sum += $twopow;
+		}
+		
+	}
+	//==================================================
+	return $powers;
+}
+
+?>
