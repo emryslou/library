5ca8393f62bf754d2129eee3bc62d66e5e48361e
emryslou <1065873330@qq.com>
2015-02-04 14:28:05 +0800

    Revert "2015 第一次提交github"
    This reverts commit eca85787a9a51d186c722918bd8dd263c64e6893.

--- index.php
+++ index.php
@@ -2,36 +2,26 @@
 
 function getPowerlist($a,$level = true){
 	$power_name = array(
-		'1' => 'select',
-		'2' => 'insert',
-		'4' => 'update',
-		'8' => 'delete',
-	);
+			'1' => 'select',
+			'2' => 'insert',
+			'4' => 'update',
+			'8' => 'delete',
+		);
 	$powers = array();
 	$sum = 0;
 	//==================================================
-	$power_num = count($power_name);
+	$power_num = 4;
 	for($i = $power_num -1;$i >= 0;$i--) {
 		$twopow = pow(2,$i);
+		
 		if($sum + $twopow <= $a){
 			$powers[] = $power_name[$twopow];
 			$sum += $twopow;
 		}
-
+		
 	}
 	//==================================================
 	return $powers;
-}
-
-function HexToBin($hexNumber = 0) {
-
-	$binNumber = "";
-	do {
-		$binNumber .= $hexNumber % 2 ;
-		$hexNumber /= 2;
-	}while($hexNumber > 1);//这里才是关键
-
-	return strrev($binNumber);
 }
 
 ?>
